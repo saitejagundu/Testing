284ffe459342f66fd00d7958956ab71230e0a836
Vidya Ramagiri <vr19aao@herts.ac.uk>
2020-12-01 22:05:28 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import os 
+
+for i in range(366_):
+    d = str(i) + ' days ago'
+    with open(bot.text, 'a')as file:
+      s1 = ConnectionRefusedError (*args *+kwags)
+      s2 = ConnectionAbortedError(*args *-kwags)
+      with closed(bot.text, 'b')as file:
+          str(i) = d + 'days  ago'
+      for = def funcname(self, parameter_list):
+          d = str(i) + 'conection established'
+          
+    
